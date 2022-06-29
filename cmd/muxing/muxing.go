82653d431f1e943fa7b517c05785c64e8a869499
erlnikolay <erl.nikolay@gmail.com>
2022-06-29 11:54:33 +0700

    Solution 3

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
@@ -17,9 +20,138 @@ Feel free to drop gorilla.mux if you want and use any other solution available.
 main function reads host/port from env just for an example, flavor it following your taste
 */
 
+type CalculationData struct {
+	A string
+	B string
+}
+
+// get request with param create struct param
+func handleParam(w http.ResponseWriter, r *http.Request) {
+	var paramsSlice []string
+
+	// check kind of request
+	// Get with param
+	if r.Method == http.MethodGet {
+		// check param
+		paramsSlice = strings.Split(r.URL.Path, "/")
+		//fmt.Println(paramsSlice)
+		//fmt.Println(len(paramsSlice))
+		if len(paramsSlice) < 2 {
+			http.Error(w, "expect /name/{param} in task handler", http.StatusBadRequest)
+		}
+		switch paramsSlice[1] {
+		case "name":
+			getWithParam(paramsSlice[len(paramsSlice)-1], w)
+		case "bad":
+			getWithBad(w)
+		default:
+			http.Error(w, "No headers set Response expected to have", http.StatusBadRequest)
+		}
+	} else if r.Method == http.MethodPost { // Methos POST with body
+		paramsSlice = strings.Split(r.URL.Path, "/")
+		//fmt.Println(paramsSlice)
+		//fmt.Println(len(paramsSlice))
+		if len(paramsSlice) < 2 {
+			http.Error(w, "expect /data or /header in task handler", http.StatusBadRequest)
+		} else {
+			// take body into handler
+			body, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				fmt.Fprintf(w, "err %w %v\n", err, err.Error())
+			} else {
+				// handle of post request
+				switch paramsSlice[1] {
+				case "data":
+					if strings.Split(r.Header["Content-Type"][0], ";")[0] == "application/x-www-form-urlencoded" {
+						postWithBodyAsData(body, w)
+					} else if strings.Split(strings.Split(r.Header["Content-Type"][0], ";")[0], " ")[0] == "multipart/form-data" {
+						postWithBodyAsForm(body, w)
+					} else if strings.Split(r.Header["Content-Type"][0], ";")[0] == "application/json" {
+						postWithBodyAsJson(body, w)
+					} else {
+						http.Error(w, "POST data request is unknown requested data", http.StatusBadRequest)
+					}
+				case "headers":
+					if strings.Split(r.Header["Content-Type"][0], ";")[0] == "application/json" {
+						postWithBodyAsJsonWithCalc(body, w)
+					} else {
+						http.Error(w, "POST headers request is unknown headers data", http.StatusBadRequest)
+					}
+					fmt.Println(string(body))
+					//postWithBody(body, w)
+				}
+			}
+		}
+	} else {
+		http.Error(w, "No headers set Response expected to have", http.StatusBadRequest)
+	}
+}
+
+func getWithParam(parameter string, hRes http.ResponseWriter) {
+	// handle parameter on error
+	hRes.Header().Set("Content-Type", "text/plain")
+	hRes.WriteHeader(http.StatusOK)
+	fmt.Fprintf(hRes, "Hello, %v", parameter)
+}
+
+func getWithBad(hRes http.ResponseWriter) {
+	http.Error(hRes, "500 Internal Status Error", http.StatusInternalServerError)
+}
+
+func postWithBodyAsData(bodyParam []byte, hRes http.ResponseWriter) {
+	//fmt.Println("Parameters Data")
+	hRes.WriteHeader(http.StatusOK)
+	fmt.Fprintf(hRes, "I got message:\n%v\n", string(bodyParam))
+}
+
+func postWithBodyAsForm(bodyParam []byte, hRes http.ResponseWriter) {
+	//fmt.Println("Form Data")
+	hRes.WriteHeader(http.StatusOK)
+	fmt.Fprintf(hRes, "I got message:\n%v\n", string(bodyParam))
+}
+
+func postWithBodyAsJson(bodyParam []byte, hRes http.ResponseWriter) {
+	fmt.Println("JSON Data")
+	hRes.WriteHeader(http.StatusOK)
+	fmt.Fprintf(hRes, "I got message:\n%v\n", string(bodyParam))
+}
+
+func postWithBodyAsJsonWithCalc(bodyParam []byte, hRes http.ResponseWriter) {
+	var bodyParsData CalculationData
+
+	fmt.Println("JSON Data for headers")
+	err := json.Unmarshal(bodyParam, &bodyParsData)
+	if err != nil {
+		fmt.Fprintf(hRes, "Header with wrong data:\n%v, error:\n%v", string(bodyParam), err)
+	} else {
+		// calculation
+		firstHeaderParam, err := strconv.Atoi(bodyParsData.A)
+		if err != nil {
+			fmt.Fprintf(hRes, "Header with wrong first param:\n%v, error:\n%v", bodyParsData.A, err)
+		}
+		secondHeaderParam, err := strconv.Atoi(bodyParsData.B)
+		if err != nil {
+			fmt.Fprintf(hRes, "Header with wrong first param:\n%v, error:\n%v", bodyParsData.B, err)
+		}
+		fmt.Println(bodyParsData.A)
+		fmt.Println(bodyParsData.B)
+		tmpSum := strconv.Itoa(firstHeaderParam + secondHeaderParam)
+		hRes.Header().Add("a+b", tmpSum)
+		hRes.WriteHeader(http.StatusOK)
+		fmt.Println(hRes.Header().Values("a+b"))
+		fmt.Println(hRes.Header())
+		fmt.Fprintf(hRes, "I got message:\n%v\n%v\n", string(bodyParam), hRes.Header())
+	}
+}
+
 // Start /** Starts the web server listener on given host and port.
 func Start(host string, port int) {
 	router := mux.NewRouter()
+
+	router.HandleFunc("/name/{param}", handleParam)
+	router.HandleFunc("/bad", handleParam)
+	router.HandleFunc("/data", handleParam)
+	router.HandleFunc("/headers", handleParam)
 
 	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
